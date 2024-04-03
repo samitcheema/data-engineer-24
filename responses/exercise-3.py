88b6a9e02a74af2d1cebc3c1ad7746905ff33387
sami cheema <samitcheema@gmail.com>
2024-04-03 07:10:07 -0400

    exercise 3

--- responses/exercise-3.py
+++ responses/exercise-3.py
@@ -1,4 +1,25 @@
-# write your code here
+import pyspark.sql import SparkSession
 
-if __name__ == "__main__":
-    print("hello world")
+spark = SparkSession.builder \
+    .appName("local-de-test") \
+    .getOrCreate()
+
+table_history_df = spark.read.csv("table-history.csv")
+status_changes_df = spark.read.csv("status_changes_dataset.csv")
+
+combined_df = status_changes_df.join(table_history_df,"customer_id")
+
+
+WindowFunc = Window.partitionBy("customer_id").orderBy("start_date")
+
+combined_df = combined_df.withColumn("next_start_date", lead("start_date").over(windowSpec))
+combined_df = combined_df.withColumn("end_reason", lead("status").over(windowSpec))
+
+
+combined_df = combined_df.withColumn(
+    "membership_period_end_reason",
+    when(col("status") == "FREEZE", "freeze").when(col("status") == "TERMINATE", "termination")
+) # time constraint - add other checks
+
+# use this to format history table
+def parse_ledger(ledger_str):
