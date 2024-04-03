9d94e5be4ec01d44b0886510363eeee4244a78b5
sami cheema <samitcheema@gmail.com>
2024-04-03 06:27:11 -0400

    exercise-1 complete

--- responses/exercise-1.sql
+++ responses/exercise-1.sql
@@ -1,2 +1,18 @@
--- write your SQL here
+SELECT 
+    oi.locationId,
+    MONTH(t.datetime) AS Month,
+    SUM(CAST(JSON_VALUE(t.details, '$.items[0].amount') AS DECIMAL(10,2))) AS RefundAmount
+FROM 
+    orderItems oi 
+JOIN 
+    transactions t
+ON oi.id = JSON_VALUE(t.details, '$.items[0].id')
+WHERE 
+	t.type = 'refund'
+GROUP BY 
+    oi.locationId, 
+    MONTH(t.datetime);
+
+
+
 select 1;
